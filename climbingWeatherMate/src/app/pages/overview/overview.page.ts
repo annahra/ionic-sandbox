9391eced7d859c4c0d1109a596e995a9c5282d85
auna <annah.ramones@gmail.com>
2020-07-02 16:17:45 -0700

    [annah] added ability to add, remove and store cities to the app

--- climbingWeatherMate/src/app/pages/overview/overview.page.ts
+++ climbingWeatherMate/src/app/pages/overview/overview.page.ts
@@ -3,6 +3,10 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Platform, LoadingController, AlertController, IonSlides } from '@ionic/angular';
 import { WeatherService } from 'src/app/services/weather.service';
 import { forkJoin } from 'rxjs';
+import { Storage } from '@ionic/storage';
+
+
+const CITIES_KEY = 'cities';
 
 @Component({
   selector: 'app-overview',
@@ -24,7 +28,7 @@ export class OverviewPage implements OnInit {
 
   constructor(private geolocation: Geolocation, private platform: Platform,
     private weatherService: WeatherService, private loadingCtrl: LoadingController,
-    private alertCtrl: AlertController) { }
+    private alertCtrl: AlertController, private storage: Storage) { }
 
   ngOnInit() {
     this.platform.ready().then(() => {
@@ -32,6 +36,7 @@ export class OverviewPage implements OnInit {
         console.log('test: ', position);
         this.entries.push({ type: 'geo', val: position.coords, class: 'cold' });
         this.loadWeather(0);
+        this.loadCities();
       }, err => {
         console.log('position error: ', err);
       })
@@ -150,6 +155,7 @@ export class OverviewPage implements OnInit {
           handler: (data) => {
             let city = { type: 'city', val: data.name, nextDays: [], id: new Date().getTime(), class: 'cold' };
             this.entries.push(city);
+            this.storeCity(city);
 
             setTimeout(() => {
               this.slides.slideTo(this.entries.length, 200);
@@ -165,6 +171,46 @@ export class OverviewPage implements OnInit {
     this.slides.getActiveIndex().then(index => {
       this.loadWeather(index);
     })
+  }
+
+  storeCity(city) {
+    this.storage.get(CITIES_KEY).then(res => {
+      if (!res) {
+        this.storage.set(CITIES_KEY, [city]);
+      } else {
+        res.push(city);
+        this.storage.set(CITIES_KEY, res);
+      }
+    });
+  }
+
+  loadCities() {
+    this.storage.get(CITIES_KEY).then(res => {
+      if(res) {
+        this.entries.push(...res);
+        for(let i = 1; i < this.entries.length; i++) {
+          this.loadWeather(i);
+        }
+      }
+    });
+  }
+
+  removeCity(index) {
+    console.log('remove: ', index);
+    let toRemove = this.entries[index].id;
+    let copy = Object.create(this.entries);
+
+    this.entries = [];
+
+    setTimeout(() => {
+      this.entries = copy.filter(entry => entry.id != toRemove);
+      this.slides.slideTo(index - 1, 200);
+    }, 10);
+
+    this.storage.get(CITIES_KEY).then(res => {
+      let toKeep = res.filter(entry => entry.id != toRemove);
+      this.storage.set(CITIES_KEY, toKeep);
+    });
   }
 
 }
