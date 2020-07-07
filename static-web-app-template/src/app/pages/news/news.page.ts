c525213aeaf3e36831b282e60c08f3b7d1ed1257
auna <annah.ramones@gmail.com>
2020-07-07 16:44:34 -0700

    [annah] added SEO but only google can pick up on it because the meta tags come up after loading on client side

--- static-web-app-template/src/app/pages/news/news.page.ts
+++ static-web-app-template/src/app/pages/news/news.page.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { ScreenSizeService } from '../../services/screen-size.service';
 import data from './news.json';
 import { Platform } from '@ionic/angular';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-news',
@@ -17,13 +18,29 @@ export class NewsPage implements OnInit {
 
   constructor(
     private screenSizeService: ScreenSizeService,
-    private plt: Platform
+    private plt: Platform,
+    private metaService: Meta,
+    private titleService: Title
   ) {
     this.isDesktop = this.screenSizeService.isDesktopView();
    }
 
   ngOnInit() {
     this.platforms = this.plt.platforms();
+
+    this.titleService.setTitle('Generic Title');
+    this.metaService.updateTag({ name: 'description', content: 'Ionic everywhere news page' });
+        // Twitter
+    this.metaService.updateTag({ property: 'twitter:card', content: 'summary_large_image' });
+    this.metaService.updateTag({ property: 'twitter:title', content: 'My Ionic everywhere news page' });
+    this.metaService.updateTag({ property: 'twitter:description', content: 'My Twitter news description!' });
+    this.metaService.updateTag({ property: 'twitter:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+    // Facebook
+    this.metaService.updateTag({ property: 'og:url', content: '/news' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:description', content: 'My OG news description!' });
+    this.metaService.updateTag({ property: 'og:title', content: 'My OG news title!' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
   }
 
 }
--- static-web-app-template/src/app/pages/web-home/web-home.page.ts
+++ static-web-app-template/src/app/pages/web-home/web-home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-web-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebHomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private metaService: Meta, 
+    private titleService: Title
+  ) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Generic Title');
+    this.metaService.updateTag({ name: 'description', content: 'Ionic everywhere news page' });
+        // Twitter
+    this.metaService.updateTag({ property: 'twitter:card', content: 'summary_large_image' });
+    this.metaService.updateTag({ property: 'twitter:title', content: 'My Ionic everywhere news page' });
+    this.metaService.updateTag({ property: 'twitter:description', content: 'My Twitter news description!' });
+    this.metaService.updateTag({ property: 'twitter:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
+    // Facebook
+    this.metaService.updateTag({ property: 'og:url', content: '/news' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:description', content: 'My OG news description!' });
+    this.metaService.updateTag({ property: 'og:title', content: 'My OG news title!' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://i0.wp.com/devdactic.com/wp-content/uploads/2020/05/ionic-in-app-purchase-capacitor.png?w=1620&ssl=1' });
   }
 
 }
